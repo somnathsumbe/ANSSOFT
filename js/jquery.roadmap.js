12e6a0271df7810405c31953631e940162ed1c48
Somnath Sumbe <P10475242@capita.co.uk>
2020-02-05 17:03:00 +0530

    2 commit

--- js/jquery.roadmap.js
+++ js/jquery.roadmap.js
@@ -12,18 +12,6 @@
 
 	"use strict";
 
-	// undefined is used here as the undefined global variable in ECMAScript 3 is
-	// mutable (ie. it can be changed by someone else). undefined isn't really being
-	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-	// can no longer be modified.
-
-	// window and document are passed through as local variables rather than global
-	// as this (slightly) quickens the resolution process and can be more efficiently
-	// minified (especially when both are regularly referenced in your plugin).
-
-	/**
-  * jQuery custom plugin implement the roadmap functionality
-  */
 
 	$.fn.roadmap = function (events, opts) {
 		if (!events instanceof Array) {
