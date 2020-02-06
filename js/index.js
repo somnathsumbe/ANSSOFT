9fe883ea146812351c3f4f15a93a1ef283c5c7d0
Somnath Sumbe <P10475242@capita.co.uk>
2020-02-07 01:24:39 +0530

    mail

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 function sendEmail() {
+	var name = document.getElementById("name").innerHTML = name;
+	console.log(name);
 	Email.send({
+		
 	Host: "smtp.gmail.com",
-	Username : "somasumbe@gmail.com",
-	Password : "Shivansh@9028",
-	To : 'somasumbe@gmail.com',
-	From : "somasumbe@gmail.com",
-	Subject : "gurukul",
-	Body : "hii",
+	Username : "anssoftware2013@gmail.com",
+	Password : "Anssoftware@2013",
+	To : 'anssoftware2013@gmail.com',
+	From : "anssoftware2013@gmail.com",
+	Subject : "gurukul i how are you ",
+	Body : "hii hii  sdasda  da da d  adada   dadad",
 	}).then(
 		message => alert("mail sent successfully")
 	);
