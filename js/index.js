a4344b262c4b5884492d8327ea32038e1b0a8df1
Somnath Sumbe <P10475242@capita.co.uk>
2020-02-07 01:43:51 +0530

    mail code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 function sendEmail() {
-	var name = document.getElementById("name").innerHTML = name;
-	console.log(name);
+	// var name = document.getElementById("name").value;
+	// var email = document.getElementById("email").value;
+	// var subject = document.getElementById("subject").value;
+	// var message = document.getElementById("Message").value;
 	Email.send({
 		
 	Host: "smtp.gmail.com",
@@ -8,8 +10,8 @@ function sendEmail() {
 	Password : "Anssoftware@2013",
 	To : 'anssoftware2013@gmail.com',
 	From : "anssoftware2013@gmail.com",
-	Subject : "gurukul i how are you ",
-	Body : "hii hii  sdasda  da da d  adada   dadad",
+	Subject : "sas",
+	Body :"di",
 	}).then(
 		message => alert("mail sent successfully")
 	);
