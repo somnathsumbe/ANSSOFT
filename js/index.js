8ae42f9917298e7b3970a1c241692f4a53059d92
Somnath Sumbe <P10475242@capita.co.uk>
2020-02-07 15:48:46 +0530

    alter impliment

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function sendEmail() {
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
-	var subject = document.getElementById("subject").value;
+	var mobile = document.getElementById("subject").value;
 	var mass = document.getElementById("mass").value;
 	Email.send({
 		Host: "smtp.gmail.com",
@@ -13,6 +13,9 @@ function sendEmail() {
 		Body: " Name: "+ name+ "</br>" + " Email: "+ email+" </br>" +" Massage: "+ mass ,
 		
 	}).then(
-		message => alert("mail sent successfully")
+		 message=>"mail sent successfully"
 	);
-}
+		alert('mail sent successfully');
+  }
+
+
