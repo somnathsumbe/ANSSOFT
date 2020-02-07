bfa056660feadece10245563dfd0b359e0fda9fb
Somnath Sumbe <P10475242@capita.co.uk>
2020-02-07 20:57:55 +0530

    23

--- js/index.js
+++ js/index.js
@@ -1,20 +1,24 @@
 function sendEmail() {
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
-	var mobile = document.getElementById("subject").value;
+	var subject = document.getElementById("subject").value;
 	var mass = document.getElementById("mass").value;
+	var mobile = document.getElementById("mobile").value;
 	Email.send({
 		Host: "smtp.gmail.com",
 		Username: "anssoftware2013@gmail.com",
 		Password: "Anssoftware@2013",
 		To: 'anssoftware2013@gmail.com',
 		From: "anssoftware2013@gmail.com",
-		Body: " Name: "+ name+ "" + "Mobile Number:"+mobile+" Email: "+ email+"" +" Massage: "+ mass ,
+		Subject:subject,
+		Body: " Name : "+ name + " " + " Mobile : "+ mobile + " Email: "+ email + " " +" Massage: "+ mass ,
 		
 	}).then(
-		 message=>"mail sent successfully"
+		 message=> alert("mail sent successfully")
+
 	);
-		alert('mail sent successfully');
-  }
+	alert("mail sent successfully");
+
+}
 
 
